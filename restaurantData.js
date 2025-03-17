bf6f537d830b24c5cc54ef9b13ab29cf7af0a589
Kulkarni Sai Pooja <poojakulkarni2123@gmail.com>
2025-03-17 21:47:16 +0530

    feat: swiggy layout

---
+++ restaurantData.js
@@ -0,0 +1,44 @@
+const restaurants = [
+    {
+        name: "Meghana's Kitchen",
+        rating: "4.3 Stars",
+        cuisine: "Arabic, Jain",
+        price: "400 For Two",
+        deliveryTime: "38 minutes",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a2zbqcyioz0wxi2ihdmc"
+    },
+    {
+        name: "Spice Delight",
+        rating: "4.5 Stars",
+        cuisine: "Indian, Chinese",
+        price: "350 For Two",
+        deliveryTime: "30 minutes",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a2zbqcyioz0wxi2ihdmc"
+    },
+    {
+        name: "Urban Tandoor",
+        rating: "4.2 Stars",
+        cuisine: "North Indian, Mughlai",
+        price: "450 For Two",
+        deliveryTime: "40 minutes",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a2zbqcyioz0wxi2ihdmc"
+    },
+    {
+        name: "Tandoori Flames",
+        rating: "4.0 Stars",
+        cuisine: "BBQ, Indian",
+        price: "500 For Two",
+        deliveryTime: "42 minutes",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a2zbqcyioz0wxi2ihdmc"
+    },
+    {
+        name: "Saffron Bites",
+        rating: "4.6 Stars",
+        cuisine: "Continental, Italian",
+        price: "600 For Two",
+        deliveryTime: "35 minutes",
+        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a2zbqcyioz0wxi2ihdmc"
+    }
+];
+
+export default restaurants;
